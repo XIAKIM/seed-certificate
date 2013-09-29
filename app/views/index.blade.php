3bd843d1b28318804dab9ab270cbddf346187760
Pawis Chairoj <pawis.joe@gmail.com>
2013-09-29 15:43:28 +0700

    Update view template and add bootstrap framework

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -2,10 +2,36 @@
 <html>
 <head>
 	<title>Seed Certificate</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<!-- stylesheet -->
+	<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap-theme.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 <body>
-	<header><h1>This is HEADER.</h1></header>
-	@yield('content')
-	<footer><h2>This is FOOTER.</h2></footer>
+	<!-- header -->
+	<header>
+		<h1>This is HEADER.</h1>
+	</header>	
+	<!-- end header -->
+
+
+	<!-- content -->
+	<div class="container">
+		@yield('content')
+	</div>
+	<!-- end content -->
+
+
+	<!-- footer -->
+	<footer>
+		<h2>This is FOOTER.</h2>
+	</footer>
+	<!-- end footer -->
+
+
+	<!-- javascript library -->
+	<script type="text/javascript" src="js/jquery-2.0.3.js"></script>
+	<script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
 </body>
 </html>
