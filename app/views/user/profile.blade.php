b092775dc1001c642b65739ffe17a3e749666587
piyawut615 <piyawut615@gmail.com>
2013-09-30 20:00:24 +0700

    add profile page

--- app/views/user/profile.blade.php
+++ app/views/user/profile.blade.php
@@ -1,4 +1,42 @@
 @extends('index')
 @section('content')
-	profile.blade
+		<img src="" alt="" class="img-thumbnail">
+	<table class="table">
+		<tr>
+			<td>Name</td>
+			<td>aaa</td>
+		</tr>
+		<tr>
+			<td>Age</td>
+			<td></td>
+		</tr>
+		<tr>
+			<td>Nationality</td>
+			<td></td>
+		</tr>
+		<tr>
+			<td>Nationality Type</td>
+			<td></td>
+		</tr>
+		<tr>
+			<td>Nationality Number</td>
+			<td></td>
+		</tr>
+		<tr>
+			<td>Issued District</td>
+			<td></td>
+		</tr>
+		<tr>
+			<td>Issued Province'</td>
+			<td></td>
+		</tr>
+		<tr>
+			<td>Address</td>
+			<td></td>
+		</tr>
+		<tr>
+			<td>Tel Number</td>
+			<td></td>
+		</tr>
+	</table>
 @stop
