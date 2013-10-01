3caffaf2f63217bffcfe3ca8047086ad3920239a
piyawut615 <piyawut615@gmail.com>
2013-10-01 15:29:01 +0700

    firsr page's user by phoom

---
+++ app/views/user/firstUser.blade.php
@@ -0,0 +1,13 @@
+@extends('index')
+@section('content')
+
+<div class="container">
+		<h2 id='Name'>Hello, Mr. Piyawut Kamwiset.</h2>	
+		<p>You don't have any request pending.</p>	
+		<p>If you want to request for any certification, please click on the "Request" button.</p>	
+		<br></br>
+		<a href="#" class="btn btn-primary btn-lg">Request</a>
+</div>
+
+
+@stop
