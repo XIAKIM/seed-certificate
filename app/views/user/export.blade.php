690a1c2971fc9ac4874ef6933dfdcc0d35207338
piyawut615 <piyawut615@gmail.com>
2013-10-01 13:59:41 +0700

    complete inport /export

--- app/views/user/export.blade.php
+++ app/views/user/export.blade.php
@@ -5,17 +5,42 @@
 </div>
 
 
+<div class="row">
+  <div class="span6">
+    <div class="col-lg-6">
+      <div class="input-group">
+        <input id='amount' type="text" class="form-control">
+        <span class="input-group-btn">
+          <button onclick='addRows()' class="btn btn-default" type="button">Go!</button>
+        </span>
+      </div>
+    </div>
 
-<table class="table">
-  <td>
-  	<tr>No</tr>
-  	<tr>Type</tr>
-  	<tr>Name</tr>
-  	<tr>ScienceName</tr>
-  	<tr>Unit</tr>
-  	<tr>Origin</tr>
-  	<tr>Amount</tr>
-  <td>
+  </div>
+  <div class='span5'></div>
+  <div class="span1">
+
+    <button type="reset" class="btn btn-default">Reset</button>
+  </div>
+
+</div>
+
+<table id='table' class="table">
+  <tr id='addRow'>
+    <td>No</td>
+    <td>Type</td>
+    <td>Name</td>
+    <td>ScienceName</td>
+    <td>Unit</td>
+    <td>Origin</td>
+    <td>Amount</td>
+  </tr>
 </table>
+
+<div class="btn-group">
+  <button type="button" class="btn btn-default">Back</button>
+  <button type="button" class="btn btn-default">Submit</button>
+
+</div>
 
 @stop
--- app/views/user/import.blade.php
+++ app/views/user/import.blade.php
@@ -1,5 +1,46 @@
 @extends('index')
 @section('content')
 
+ <div class="page-header">
+  <h1>Import List</h1>
+</div>
 
+
+<div class="row">
+    <div class="span6">
+<div class="col-lg-6">
+    <div class="input-group">
+      <input id='amount' type="text" class="form-control">
+      <span class="input-group-btn">
+        <button onclick='addRows()' class="btn btn-default" type="button">Go!</button>
+      </span>
+    </div>
+  </div>
+
+    </div>
+    <div class='span5'></div>
+    <div class="span1">
+
+  <button type="reset" class="btn btn-default">Reset</button>
+    </div>
+
+</div>
+
+    <table id='table' class="table">
+    <tr id='addRow'>
+      <td>No</td>
+      <td>Type</td>
+      <td>Name</td>
+      <td>ScienceName</td>
+      <td>Unit</td>
+      <td>Origin</td>
+      <td>Amount</td>
+    </tr>
+  </table>
+
+<div class="btn-group">
+  <button type="button" class="btn btn-default">Back</button>
+  <button type="button" class="btn btn-default">Submit</button>
+
+</div>
 @stop
