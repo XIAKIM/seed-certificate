555d4f8b031f0369905d4c5a6f95a75ac2198975
piyawut615 <piyawut615@gmail.com>
2013-10-01 10:11:42 +0700

    add export tem

---
+++ app/views/user/export.blade.php
@@ -0,0 +1,21 @@
+@extends('index')
+@section('content')
+<div class="page-header">
+  <h1>Export List</h1>
+</div>
+
+
+
+<table class="table">
+  <td>
+  	<tr>No</tr>
+  	<tr>Type</tr>
+  	<tr>Name</tr>
+  	<tr>ScienceName</tr>
+  	<tr>Unit</tr>
+  	<tr>Origin</tr>
+  	<tr>Amount</tr>
+  <td>
+</table>
+
+@stop
