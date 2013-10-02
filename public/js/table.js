4ce6fcb98646ccd5996c41884f37ff39818b9e9f
piyawut615 <piyawut615@gmail.com>
2013-10-02 12:36:28 +0700

    create  seedVerificationAll page by mew

--- public/js/table.js
+++ public/js/table.js
@@ -12,36 +12,33 @@ function addRows()
 		var newrow = document.createElement("tr");
 
 		
-		newCol1 = document.createElement("td");
-		newCol1.innerHTML = i+1;
-		newrow.appendChild(newCol1);
+		newCol0 = document.createElement("td");
+		newCol0.innerHTML = i+1;
+		newrow.appendChild(newCol0);
 	
-
-		
+		newCol1 = document.createElement("td");
+		newCol1.innerHTML = "<input type='text' name='Type"+i+"'>";
+		newrow.appendChild(newCol1);
 
 		newCol2 = document.createElement("td");
-		newCol2.innerHTML = "<input type='text' name='Type'>";
+		newCol2.innerHTML = "<input type='text' name='Name"+i+"'>";
 		newrow.appendChild(newCol2);
 
 		newCol3 = document.createElement("td");
-		newCol3.innerHTML = "<input type='text' name='Name'>";
+		newCol3.innerHTML = "<input type='text' name='ScienceName"+i+"'>";
 		newrow.appendChild(newCol3);
 
 		newCol4 = document.createElement("td");
-		newCol4.innerHTML = "<input type='text' name='ScienceName'>";
+		newCol4.innerHTML = "<input type='text' name ='Unit"+i+"'>";
 		newrow.appendChild(newCol4);
 
 		newCol5 = document.createElement("td");
-		newCol5.innerHTML = "<input type='text' name ='Unit'>";
+		newCol5.innerHTML = "<input type='text' name ='Origin"+i+"'>";
 		newrow.appendChild(newCol5);
 
 		newCol6 = document.createElement("td");
-		newCol6.innerHTML = "<input type='text' name ='Origin<'>";
+		newCol6.innerHTML = "<input type='text' name ='Amount"+i+"'>";
 		newrow.appendChild(newCol6);
-
-		newCol7 = document.createElement("td");
-		newCol7.innerHTML = "<input type='text' name ='Amount'>";
-		newrow.appendChild(newCol7);
 
 		node.appendChild(newrow);
 	}
