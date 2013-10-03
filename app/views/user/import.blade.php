e561640e11a850665cfa63fb441cdb0ea279dee0
piyawut615 <piyawut615@gmail.com>
2013-10-03 21:16:21 +0700

    chang function one press per one seed in import and update js

--- app/views/user/import.blade.php
+++ app/views/user/import.blade.php
@@ -1,43 +1,57 @@
 @extends('index')
 @section('content')
 
- <div class="page-header">
+<div class="page-header">
   <h1>Import List</h1>
 </div>
 
 
 <div class="row">
   <div class="col-md-6">
-    <div class="col-lg-6">
-      <div class="input-group">
-        <input id='amount' type="text" class="form-control">
-        <span class="input-group-btn">
-          <button onclick='addRows()' class="btn btn-default" type="button">Go!</button>
-        </span>
-      </div><!-- /input-group -->
-    </div><!-- /.col-lg-6 -->
+   <!-- Button trigger modal -->
+   <button   data-toggle="modal" href="#formModel"class="btn btn-default" type="button">Add!</button>
+   <!-- Modal -->
+   <div class="modal fade" id="formModel" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+
+          <h4 class="modal-title">Modal title</h4>
+        </div>
+        <div class="modal-body">
+            ......
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+          <button onclick='addRowsOfImport()' type="button" class="btn btn-primary" data-dismiss="modal">Save changes</button>
+        </div>
+      </div><!-- /.modal-content -->
+    </div><!-- /.modal-dialog -->
+  </div><!-- /.modal -->
+
+</div>
 
 
-  </div>
-  <div class="col-md-4"></div>
 
-  <div class="col-md-2">
+<div class="col-md-4"></div>
+
+<div class="col-md-2">
  <button type="reset" class="btn btn-default">Reset</button>
-  </div>
+</div>
 </div>
 <br>
 
-    <table id='table' class="table">
-    <tr id='addRow'>
-      <td>No</td>
-      <td>Type</td>
-      <td>Name</td>
-      <td>ScienceName</td>
-      <td>Unit</td>
-      <td>Origin</td>
-      <td>Amount</td>
-    </tr>
-  </table>
+<table id='table' class="table">
+  <tr id='addRow'>
+    <th width='100px'>No</th>
+    <th>Type</th>
+    <th>Name</th>
+    <th>ScienceName</th>
+    <th>Unit</th>
+    <th>Origin</th>
+    <th>Amount</th>
+  </tr>
+</table>
 
 <div class="btn-group">
   <button type="button" class="btn btn-default">Back</button>
--- public/js/table.js
+++ public/js/table.js
@@ -1,16 +1,51 @@
-function addRows()
-{
+var numImport=0;
+var numExport=0;
 
-	var num = document.getElementById('amount').value;
-	
-		
-	for(var i=0;i<num;i++)
-	{
+
+
+function addRowsOfImport(){
 
 		var numCol = new Array();
 		var node = document.getElementById('table').childNodes[1];
 		var newrow = document.createElement("tr");
+		
+		newCol0 = document.createElement("td");
+		newCol0.innerHTML = numImport+1;
+		newrow.appendChild(newCol0);
+	
+		newCol1 = document.createElement("td");
+		// newCol1.innerHTML = "<label id='"+numImport"'></label>";
+		// newCol1.innerHTML = "<input type='text' name='Type"+numImport+"'>";
+		newrow.appendChild(newCol1);
 
+		newCol2 = document.createElement("td");
+		newCol2.innerHTML = "<input type='text' name='Name"+numImport+"'>";
+		newrow.appendChild(newCol2);
+
+		newCol3 = document.createElement("td");
+		newCol3.innerHTML = "<input type='text' name='ScienceName"+numImport+"'>";
+		newrow.appendChild(newCol3);
+
+		newCol4 = document.createElement("td");
+		newCol4.innerHTML = "<input type='text' name ='Unit"+numImport+"'>";
+		newrow.appendChild(newCol4);
+
+		newCol5 = document.createElement("td");
+		newCol5.innerHTML = "<input type='text' name ='Origin"+numImport+"'>";
+		newrow.appendChild(newCol5);
+
+		newCol6 = document.createElement("td");
+		newCol6.innerHTML = "<input type='text' name ='Amount"+numImport+"'>";
+		newrow.appendChild(newCol6);
+
+		node.appendChild(newrow);
+		numImport++;
+}
+
+function addRowsOfExport()
+{
+		var node = document.getElementById('table').childNodes[1];
+		var newrow = document.createElement("tr");
 		
 		newCol0 = document.createElement("td");
 		newCol0.innerHTML = i+1;
@@ -41,5 +76,5 @@ function addRows()
 		newrow.appendChild(newCol6);
 
 		node.appendChild(newrow);
-	}
+		numExport++;
 }
