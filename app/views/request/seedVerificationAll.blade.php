308b86da4fe6ad4dd579b24d5b9b7c76ea226d0f
Pawis Chairoj <pawis.joe@gmail.com>
2013-10-03 21:42:01 +0700

    change UI on seedVerificationAll.blade.hp

--- app/views/request/seedVerificationAll.blade.php
+++ app/views/request/seedVerificationAll.blade.php
@@ -1,33 +1,50 @@
 @extends('index')
 @section('content')
-	<div class="container">
-  <h1>Seed Verification</h1>
-  <div class="bs-example">
-      <table class="table table-bordered">
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Status</th>
-
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Pawart Narujjiroj</td>
-            <td><a href="">Waiting</a></td>
-
-          </tr>
-          <tr>
-            <td>Pawis Chairoj</td>
-            <td > <a href="">Waiting</a></td>
-
-          </tr>
-          <tr>
-            <td>Piyanggoon</td>
-            <td><a href="">Waiting</a></td>
-
-          </tr>
-        </tbody>
-      </table>
+	<div class="panel-group" id="accordion">
+  <div class="panel panel-default">
+    <div class="panel-heading">
+      <h4 class="panel-title">
+        <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#collapseOne">
+          - PP1
+        </a>
+      </h4>
     </div>
+    <div id="collapseOne" class="panel-collapse collapse in">
+      <div class="panel-body">
+        <h2>Artima Mahahemarat</h2>
+        <h2>Piyawut Kamwiset</h2>
+      </div>
+    </div>
+  </div>
+  <div class="panel panel-default">
+    <div class="panel-heading">
+      <h4 class="panel-title">
+        <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo">
+          - PP8
+        </a>
+      </h4>
+    </div>
+    <div id="collapseTwo" class="panel-collapse collapse">
+      <div class="panel-body">
+        <h2>Artima Mahahemarat</h2>
+        <h2>Piyawut Kamwiset</h2>
+      </div>
+    </div>
+  </div>
+  <div class="panel panel-default">
+    <div class="panel-heading">
+      <h4 class="panel-title">
+        <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#collapseThree">
+         - PP9
+        </a>
+      </h4>
+    </div>
+    <div id="collapseThree" class="panel-collapse collapse">
+      <div class="panel-body">
+        <h2>Artima Mahahemarat</h2>
+        <h2>Piyawut Kamwiset</h2>
+      </div>
+    </div>
+  </div>
+</div>
 @stop
