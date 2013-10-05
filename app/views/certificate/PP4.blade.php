01e8a9dc3bee31d2481bee6459d52c9844a59d96
piyawut615 <piyawut615@gmail.com>
2013-10-05 16:09:10 +0700

    Add pp3 pp4   by most

---
+++ app/views/certificate/PP4.blade.php
@@ -0,0 +1,44 @@
+@extends('index')
+@section('content')
+
+
+			<div class="img-resize"><img src="http://www.siamganesh.com/all_gods/new/garuda/garuda3.jpg" class="centerimg">
+			</div>
+			<h2 class="textcenter">Seed Selling Certification</h2>
+			<label class="pull-left">Certificate No. .....</label>
+			<label class="pull-right">Department of Agriculture</label>
+			<br />
+			<center>
+				<label>This cerificate for</label>
+				<br />
+				.........................................................................................................
+			</center>
+			<label>Company address</label>...................<label>Moo</label>.....<label>Soi</label>.....<label>Road</label>...................
+			<br />
+			<label>Tambol</label>................... <label>District</label>................... <label>Province</label>...................
+			<br />
+			<label>Owner</label>............................................................................
+			<p>
+				This certification allows the owner for selling seed follow matra 14 of seed enactment 2518 B.E. reform again in seed enactment 2535 B.E.(second copy) and seed lists that had already requested to the officer.
+			</p>
+			<br />
+			
+			<label>Storage Location No.</label>................... <label>Moo</label>................... <label>Soi</label>................... <label>Road</label>...................
+			<br />
+			<br />
+			<label>This certification is available on</label>................... <label>Month</label>................... <label>B.E.</label>...................
+			<br />
+			<label>can be used until 31 December</label>................... <label>and can be used only the location that states in this certification.</label>................... 
+			<br />
+			
+			
+			
+			<div class="pull-right">
+				<label>(signature)</label> ............... <label>officer</label>
+				<center>
+					(.........................)
+				</center>
+				
+			</div>
+
+@stop
