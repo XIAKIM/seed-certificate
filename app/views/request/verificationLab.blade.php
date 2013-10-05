eb81f7a378de22f398511a4afd62fb310ee9264e
piyawut615 <piyawut615@gmail.com>
2013-10-05 12:30:37 +0700

    add verificationLab  by new

---
+++ app/views/request/verificationLab.blade.php
@@ -0,0 +1,23 @@
+@extends('index')
+@section('content')
+
+<h1>Lab Verification</h1>
+
+<br>
+<div class="row">
+	<div class="col-sm-offset-4 col-sm-4">	
+		<p><button type="button" class="btn btn-default btn-lg btn-block">Import Verification</button></p>	
+	</div>
+</div>
+<br><br>
+<div class="row">
+	<div class="col-sm-offset-4 col-sm-4">	
+		<p><button type="button" class="btn btn-default btn-lg btn-block">Export Verification</button></p>	
+	</div>
+</div>
+<br>
+<div class="row">
+	<div class="col-sm-offset-8 col-sm-2"> <button type="button" class="btn btn-default">Back</button> </div> 
+</div>
+
+@stop
