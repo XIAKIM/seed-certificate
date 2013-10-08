4f653050b8515ac50b240df172060bf31ed451ee
piyawut615 <piyawut615@gmail.com>
2013-10-08 18:36:58 +0700

    status page

---
+++ app/views/request/status.blade.php
@@ -0,0 +1,36 @@
+@extends('index')
+@section('content')
+
+<div class="page-header">
+  <h1>Status</h1>
+</div>
+
+<table class="table">
+  <tr>
+  		<th>Type</th>
+  		<th>Status</th>
+  </tr>
+
+  <tr>
+  		<td>PP3</td>
+  		<td>Wait</td>
+
+  </tr>
+
+  <tr>
+  		<td>PP4</td>
+  		<td>Approve</td>
+  </tr>
+
+  <tr>
+  		<td>PP8</td>
+  		<td>Wait</td>
+  </tr>
+
+  <tr>
+  		<td>PP9</td>
+  		<td>Deny</td>
+  </tr>
+</table>
+
+@stop
