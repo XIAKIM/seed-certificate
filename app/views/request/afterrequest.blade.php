3b25df6d4b951a7da5b88b413e5d93ab08311b47
Pawis Chairoj <pawis.joe@gmail.com>
2013-10-09 11:43:37 +0700

    change text in afterRequest.blade.php

--- app/views/request/afterrequest.blade.php
+++ app/views/request/afterrequest.blade.php
@@ -4,7 +4,7 @@
 <div class="page-header">
   <h1>Request Successful</h1>
 </div>
-<p><h4>Please wait </h4>for system approve your request, you can follow progress in status page.</p>
+<p><h4>Please wait </h4>for system approve your request, you can follow progress in status page</p>
 
 
 @stop
