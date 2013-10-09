e480e985b4336f91eb5443b00d369fec8f4e8faf
piyawut615 <piyawut615@gmail.com>
2013-10-09 15:07:23 +0700

    update UI PP1

--- app/views/request/requestPP1.blade.php
+++ app/views/request/requestPP1.blade.php
@@ -1,35 +1,35 @@
 
 @section('content')
 <div class = "container">
-			<h1>Request Form (PP1)</h1>
-			<div class="row">
+	<h1>Request Form (PP1)</h1>
+	<div class="row">
 
+	</div>
+	<div class="row">
+		<div class="col-md-8">
+			<h2>Request for Certificate of
+				Collecting, Selling, Importing or
+				Exporting Seed (PP1)</h2>
 			</div>
-			<div class="row">
-				<div class="col-md-8">
-					<h2>Request for Certificate of
-					Collecting, Selling, Importing or
-					Exporting Seed (PP1)</h2>
-				</div>
-				<div class="col-md-4">
-					<div class="panel panel-default">
-						<div class="panel-body ">
-							Request Number: {{ $request->id }}
-							<br />
-							Approved Date: 
-							<br />
-							Officer Name:
-						</div>
-
+			<div class="col-md-4">
+				<div class="panel panel-default">
+					<div class="panel-body ">
+						Request Number: {{ $request->id }}
+						<br />
+						Approved Date: 
+						<br />
+						Officer Name:
 					</div>
-					<p>
-						Created Date: {{ $request->created_at }}
-					</p>
+
 				</div>
+				<p>
+					Created Date: {{ $request->created_at }}
+				</p>
 			</div>
-			<div class="row">
-				<div class="col-md-12">
-<p>
+		</div>
+		<div class="row">
+			<div class="col-md-12">
+				<p>
 					<label>Name:</label> {{ $description->name }} {{ $description->lastName }}
 					
 					<label>Age:</label> {{ $description->age }}
@@ -44,49 +44,76 @@
 					
 					<label>Issued Province:</label> {{ $description->issuedProvince }}
 					
-					<label>Address:</label>  {{ $description->address }}
+					<!-- <label>Address:</label>  {{ $description->address }} -->
+					<label>Address: </label>
+					{{ $description->userAddressNo }}
+					<label>Moo: </label>
+					{{ $description->userAddressMoo }}
+
+
+
+					<label>Soi: </label>
+					{{ $description->userAddressSoi }}
+					<label>Road: </label>
+					{{ $description->userAddressRoad }}				
+
+
+
+					<label>Sub District: </label>
+					{{ $description->userAddressSubDistrict }}
+					<label>District: </label>
+					{{ $description->userAddressDistrict }}
+
+
+
+					<label>Province: </label>
+					{{ $description->userAddressProvince }}
+					<label>	PostalCode: </label>
+					{{ $description->userAddressPostalCode }}
+
+
 					<br />
 					<label>Tel Number:</label> {{ $description->telNumber }}
 					<br />
 					<label>Desired Certificate Type:</label> พพ{{ $type['type'] }}.
-					
+
 					<label>Certificate of Collecting Seed</label>
 					<br />
-					</p>
-					{{ Form::open(["route" => "/createPP1Request", "role" => "form"]) }}
-					<!-- <form role="form"> -->
-						<div class="form-group">
-							<label>Company Name:</label>
-							<!-- <input type="text" class="form-control"> -->
-							 {{ Form::text("companyName", null, ["class" => "form-control"] ) }}	
-							<label>Owner Name:</label>
-							<!-- <input type="text" class="form-control"> -->
-							{{ Form::text("ownerName", null, ["class" => "form-control"] ) }}	
-						</div>
-						<div class="form-group">
-							<label>Company Address:</label>
-							<!-- <input type="textarea" class="form-control"> -->
-							{{ Form::text("companyAddress", null, ["class" => "form-control"] ) }}	
-						</div>
-						<div class="form-group">
-							<label>Collecting Location:</label>
-							<!-- <input type="textarea" class="form-control"> -->
-							{{ Form::text("collectingLocation", null, ["class" => "form-control"] ) }}	
-						</div>
-						<div class="form-group">
-							<label>Storing Address:</label>
-							<!-- <input type="textarea" class="form-control"> -->
-							{{ Form::text("storingAddress", null, ["class" => "form-control"] ) }}	
-						</div>
-						<div class="form-group">
-							<button class="btn btn-default">Reset</button>
-							{{ Form::submit('Submit', ["class" => "btn btn-primary"])}}
-						</div>
-					<!-- </form> -->
-					{{ Form::close() }}
-
+				</p>
+				{{ Form::open(["route" => "/createPP1Request", "role" => "form"]) }}
+				<!-- <form role="form"> -->
+				<div class="form-group">
+					<label>Company Name:</label>
+					<!-- <input type="text" class="form-control"> -->
+					{{ Form::text("companyName", null, ["class" => "form-control"] ) }}	
+					<label>Owner Name:</label>
+					<!-- <input type="text" class="form-control"> -->
+					{{ Form::text("ownerName", null, ["class" => "form-control"] ) }}	
 				</div>
+				<div class="form-group">
+					<label>Company Address:</label>
+					<!-- <input type="textarea" class="form-control"> -->
+					{{ Form::text("companyAddress", null, ["class" => "form-control"] ) }}	
+				</div>
+				<div class="form-group">
+					<label>Collecting Location:</label>
+					<!-- <input type="textarea" class="form-control"> -->
+					{{ Form::text("collectingLocation", null, ["class" => "form-control"] ) }}	
+				</div>
+				<div class="form-group">
+					<label>Storing Address:</label>
+					<!-- <input type="textarea" class="form-control"> -->
+					{{ Form::text("storingAddress", null, ["class" => "form-control"] ) }}	
+				</div>
+				<div class="form-group">
+					<button class="btn btn-default">Reset</button>
+					{{ Form::submit('Submit', ["class" => "btn btn-primary"])}}
+				</div>
+				<!-- </form> -->
+				{{ Form::close() }}
 
 			</div>
+
 		</div>
-@stop
+	</div>
+	@stop
