7849760c96d9d94547ae7c07c753a5dcfbafed02
piyawut615 <piyawut615@gmail.com>
2013-11-05 20:01:21 +0700

    add all detail page

---
+++ app/views/request/allRequest.blade.php
@@ -0,0 +1,15 @@
+@extends('index')
+@section('content')
+<div class="page-header">
+      <h1 id="labels">Request</h1>
+</div>
+<div class="row">
+ <div class="col-md-4" class='text-center'><a href="#">Inbox <span class="badge">42</span></a></div>
+  <div class="col-md-4">.col-md-4</div>
+  <div class="col-md-4">.col-md-4</div>
+
+</div>
+
+@stop
+
+
