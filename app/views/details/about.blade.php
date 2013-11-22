48d3a7af90e0fc8b10607db92c83d4c269243f4d
piyawut615 <piyawut615@gmail.com>
2013-11-22 23:48:38 +0700

    update about detail page

--- app/views/details/about.blade.php
+++ app/views/details/about.blade.php
@@ -5,16 +5,16 @@
 		<div class="container">
 			<div class="row">
 				<div class="col-md-4">
-					<h2>Why Seed</h2>
-					<p>TEXT Here</p>
+					<h2>Why seed certification online?</h2>
+					<p>Seed application provides easy method for users to make seed certificate request by using web application that support many benefits to users.</p>
 				</div>			
 				<div class="col-md-4">
-					<h2>How we send your request to officer</h2>
-					<p>TEXT Here</p>
+					<h2>How we send your request to officer?</h2>
+					<p>After users make request through system, system will notify officer to verify request by sending request form to the officer. After officer verified the user’s request, users can get their certificate by contacting the Agro.</p>
 				</div>			
 				<div class="col-md-4">
-					<h2>How lab verify your seed analysis</h2>
-					<p>TEXT Here</p>
+					<h2>How lab verify your seed analysis?</h2>
+					<p>After officers verify request, system will notify lab to verify your seed by computing it to the actual standards that recorded in the system database.</p>
 				</div>	
 			</div>
 		</div>
