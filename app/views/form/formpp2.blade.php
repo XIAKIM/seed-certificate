86d1d0d023c386cceb74d8a594eb3c96808a79bf
kimapiwat <sri.apiwat@gmail.com>
2013-11-25 00:58:54 +0700

    change pp2 UI

--- app/views/form/formpp2.blade.php
+++ app/views/form/formpp2.blade.php
@@ -1,9 +1,57 @@
 @extends('index')
 @section('content')
-
-<div class="head_sec text-right"> <p><strong>PP 2 </strong></p> </div>
+<div class="head_sec">
+<div class="col-md-6 text-left"> <p><strong>PP2 </strong></p> </div>
+<div class="col-md-6 text-right"> <p><strong> Request No. {{$pp2->requestID}} </p></div>
+</div>
 <h1 class="text-center">Seed Passing Request Form</h1><br>
 <div class="main">
+	<div class="row">
+		<div class="col-xs-3 text-right"><p><strong> Name : </strong></p></div>
+		<div class="col-xs-9 text-left">
+			{{Description::find(user::find($pp2->userID)->descriptionID)->name}} {{Description::find(User::find($pp2->userID)->descriptionID)->lastName}}
+		</div>
+	</div>
+	<div class="row"> 	
+		<div class="col-xs-3 text-right"><p><strong> Age : </strong></p></div> 
+		<div class="col-xs-3 text-left"> {{ Description::find(User::find($pp2->userID)->descriptionID)->age }}</div>
+		<div class="col-xs-3 text-right"><p><strong> Nationality : </strong></p> </div>
+		<div class="col-xs-3 text-left"> {{ Description::find(User::find($pp2->userID)->descriptionID)->nationality }} </div>
+	</div>
+	<div class="row"> 	
+		<div class="col-xs-3 text-right"><p><strong> Identification Type : </strong> </p></div> 
+		<div class="col-xs-3 text-left"> {{ Description::find(User::find($pp2->userID)->descriptionID)->identificationType }} </div>
+		<div class="col-xs-3 text-right"><p><strong> Identification Number : </strong> </p></div> 
+		<div class="col-xs-3 text-left"> {{ Description::find(User::find($pp2->userID)->descriptionID)->identificationNumber }} </div>
+	</div>
+    <div class="row">
+		<div class="col-xs-3 text-right"><strong>From district : </strong></div>
+		<div class="col-xs-3 text-left"> {{Description::find(User::find($pp2->userID)->descriptionID)->issuedDistrict}} </div>
+		<div class="col-xs-3 text-right"><strong>Province : </strong></div>
+		<div class="col-xs-3 text-left"> {{Description::find(User::find($pp2->userID)->descriptionID)->issuedProvince}} </div>
+  	</div>
+	<div class="row"> 	
+		<p>
+		<div class="col-xs-3 text-right"> <strong> Adress : </strong> </div> 
+		<div class="col-xs-9 text-left"> 
+			{{ Description::find(User::find($pp2->userID)->descriptionID)->userAddressNo }} 
+			m.{{ Description::find(User::find($pp2->userID)->descriptionID)->userAddressMoo }}
+			soi.{{ Description::find(User::find($pp2->userID)->descriptionID)->userAddressSoi }}        	
+			{{ Description::find(User::find($pp2->userID)->descriptionID)->userAddressRoad }}.rd
+			{{ Description::find(User::find($pp2->userID)->descriptionID)->userAddressSubDistrict }} ,
+			{{ Description::find(User::find($pp2->userID)->descriptionID)->userAddressDistrict }} ,
+			{{ Description::find(User::find($pp2->userID)->descriptionID)->userAddressProvince }} ,
+			Thailand , {{ Description::find(User::find($pp2->userID)->descriptionID)->userAddressPostalCode }} 
+		</div>
+		</p>
+	</div>
+	<div class="row">
+		<p>
+		<div class="col-xs-3 text-right"> <strong> Telephone No. : </strong> </div>
+		<div class="col-xs-9 text-left"> {{ Description::find(User::find($pp2->userID)->descriptionID)->telNumber }} </div>
+		</p>
+	</div>
+<!--
   <div class="row">
     <div class="col-md-7"><p><strong>Name</strong></p></div>
     <div class="col-md-2"><p><strong>Age</strong></p></div>
@@ -35,15 +83,31 @@
       <div class="col-md-6"><p><strong>Post code</strong></p></div>
       <div class="col-md-6"><p><strong>Phone</strong></p></div>
   </div>
+-->
+	<div class="row">
+		<p>
+		<div class="col-xs-4 text-right"><strong>want to request passing certificate on behalf of</strong></div>
+		<div class="col-xs-8 text-left"> {{$pp2->companyName}} </div>
+		</p>
+	</div>
 
-  <div class="row">
-    <div class="col-md-12"><p><strong>want to request passing certificate on behalf of</strong></p></div>
-  </div>
+	<div class="row">
+		<p>
+		<div class="col-xs-4 text-right"><p><strong>driven by</strong></p></div>
+		<div class="col-xs-8 text-left"> {{$pp2->ownerName}} </div>
+		</p>
+	</div>
+	<div class="row">
+		<div class="col-xs-3 text-right"> <strong> Company Address : </strong> </div>
+		<div class="col-xs-9 text-left"> 
+			{{ $pp2->companyAddressNo }} m.{{ $pp2->companyAddressMoo }}
+			soi.{{ $pp2->companyAddressSoi }} {{ $pp2->companyAddressRoad }}.rd
+			{{ $pp2->companyAddressSubDistrict }} , {{ $pp2->companyAddressDistrict }} , 
+			{{ $pp2->companyAddressProvince }} , Thailand {{ $pp2->companyAddressPostalCode }}
+		</div>
+	</div>
 
-  <div class="row">
-    <div class="col-md-12"><p><strong>driven by</strong></p></div>
-  </div>
-
+<!--
   <div class="row">
     <div class="col-md-4"><p><strong>Company address no.</strong></p></div>
     <div class="col-md-2"><p><strong>Moo</strong></p></div>
@@ -62,34 +126,59 @@
     <div class="col-md-6"><p><strong>Phone</strong></p></div>
   </div>
 
-  <div class="row">
-    <div class="col-md-8"><p><strong>By bringing seed from city</strong></p></div>
-    <div class="col-md-4"><p><strong>country</strong></p></div>
-  </div>
+-->
+	<div class="row">
+		<p>
+		<div class="col-xs-3 text-right"><strong>By bringing seed from city</strong></div>
+		<div class="col-md-2 text-left"> {{$pp2->arrivalCity}} </div>
+		<div class="col-md-1 text-right"><strong>country</strong></div>
+		<div class="col-md-2 text-left"> {{$pp2->arrivalCountry}} </div>
+		</p>
+	</div>
+
+	<div class="row">
+		<p>
+		<div class="col-md-3 text-right"><strong>pass through (checkpoint)</strong></div>
+		<div class="col-md-2 text-left"> {{$pp2->arrivalCheckpoint}} </div>
+		<div class="col-md-1 text-right"><strong>by</strong></div>
+		<div class="col-md-2 text-left"> {{$pp2->arrivalVehicle}} </div>
+		</p>
+	</div>
+
+	<div class="row">
+		<p>
+		<div class="col-md-3 text-right"><strong>to city</strong></div>
+		<div class="col-md-2 text-left"> {{$pp2->departureCity}} </div>
+		<div class="col-md-1 text-right"><strong>country</strong></div>
+		<div class="col-md-1 text-left"> {{$pp2->departureCountry}}</div>
+		<div class="col-md-2 text-right"><strong>Road</strong></div>
+		<div class="col-md-2 text-left"> {{$pp2->transportRoute}}</div>	
+		</p>
+	</div>
+	<div class="row">
+		<p>
+		<div class="col-md-3 text-right"><strong>pass through (checkpoint)</strong></div>
+		<div class="col-md-2 text-left"> {{$pp2->departureCheckpoint}} </div>
+		<div class="col-md-1 text-right"><strong>by</strong></div>
+		<div class="col-md-2 text-left"> {{$pp2->departureVehicle}} </div>
+		</p>
+	</div>
+
+	<div class="row">
+		<p>
+		<div class="col-md-4 text-right"><strong>Transporting and collecting seed location : </strong></div>
+		<div class="col-md-8 text-left">{{$pp2->temporayLocation}}</div>
+		</p>
+	</div>
 
   <div class="row">
-    <div class="col-md-8"><p><strong>pass through (checkpoint)</strong></p></div>
-    <div class="col-md-4"><p><strong>by (vehicle)</strong></p></div>
+	<p>
+    <div class="col-md-3 text-right"><strong>Time duration for passing seed :</strong></div>
+	<div class="col-md-1 text-left">{{$pp2->passingPeriod}} </div>
+	<div class="col-md-1 text-left"><strong>Day</strong></div>
+	</p>
   </div>
 
-  <div class="row">
-    <div class="col-md-4"><p><strong>to (city)</strong></p></div>
-    <div class="col-md-4"><p><strong>(country)</strong></p></div>
-    <div class="col-md-4"><p><strong>Road</strong></p></div>
-  </div>
-
-          
-  <div class="row">
-    <div class="col-md-12"><p><strong>Transporting and collecting seed location</strong></p></div>
-  </div>
-
-  <div class="row">
-    <div class="col-md-12"><p><strong>.....</strong></p></div>
-  </div>
-
-  <div class="row">
-    <div class="col-md-5"><p><strong>Time duration for passing seed<span class="pull-right">Day</span></strong></p></div>
-  </div>
            
   </div>
 
