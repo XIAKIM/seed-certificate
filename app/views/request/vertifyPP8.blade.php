998a5c86675604ffe4f68ae6c4c3bb3a7d9d0a03
Pawis Chairoj <pawis.joe@gmail.com>
2013-12-13 12:03:32 +0700

    fixed merge conflict

--- app/views/request/vertifyPP8.blade.php
+++ app/views/request/vertifyPP8.blade.php
@@ -3,29 +3,6 @@
 <div class="page-header">
 	<h1>Request for Import List (PP8)</h1>
 </div>
-<div class="row">
-	<div class="col-md-2 col-md-offset-8 ">Created Date: </div>
-	<div class="col-md-1 ">______________</div>
-</div>
-<br>
-<div class="row">
-	<div class="col-md-2 col-md-offset-2 ">Company Name: </div>
-	<div class="col-md-2 ">______________</div>
-	<div class="col-md-2 ">Certificate Number:</div>
-	<div class="col-md-2 ">______________</div>
-</div>
-<div class="row">
-	<div class="col-md-2 col-md-offset-2 ">Approved Date: </div>
-	<div class="col-md-2 ">______________</div>
-	<div class="col-md-2 ">Expired Date: </div>
-	<div class="col-md-2 ">______________</div>
-</div>
-<br>
-
-<<<<<<< HEAD
-<div class="page-header">
-	<h1>Request for Import List (PP8)</h1>
-</div>
 
 <table class="table table-bordered">
         <thead>
@@ -62,63 +39,6 @@
         </tbody>
       </table>
 
-  <div class="row">
-      <div class="col-md-12">
-        @if(Requests::find($pp8->requestID)->status == 'Waiting')
-          {{ Form::open(["action" => ["PP8Controller@createCertificatePP8", $pp8->id]]) }}
-              {{ Form::submit('Verify', ["class" => "btn btn-success"])}}
-          {{ Form::close() }}
-        <button data-toggle="modal" href="#denySignup" type="button" class="btn btn-danger">Deny</button>
-          <!-- Modal -->
-          <div class="modal fade" id="denySignup" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-            <div class="modal-dialog">
-              <div class="modal-content">
-               <!--  <div class="modal-header">
-                 -->  <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                  <br>
-                  <h4 class="modal-title">Comment :</h4>
-                <!-- </div> -->
-                <div class="modal-body">
-                    {{ Form::open(["action" => ["PP8Controller@denyRequestPP8", $pp8->id]]) }}
-                      {{ Form::textarea('comment', null, ["rows" => "5", "cols" => "80"]) }}
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                      {{ Form::submit('Submit', ["class" => "btn btn-primary"])}}  
-                  {{ Form::close() }}
-
-                </div>
-              </div><!-- /.modal-content -->
-            </div><!-- /.modal-dialog -->
-          </div><!-- /.modal -->
-        @endif
-      </div>
-    </div>
-@stop
-=======
-<div class="col-md-4"></div>
-
-<div class="col-md-2"></div>
-
-<br>
-
-<table id='table' class="table">
-	<tr id='addRow'>
-		<th>No</th>
-		<th>Type</th>
-		<th>Common Name</th>
-		<th>Science Name</th>
-		<th>Unit</th>
-		<th>Amount</th>
-		<th>Value</th>
-		<th>Departure<br>Country</th>
-		<th>Imported<br>Checkpoint</th>
-		<th>Imported<br>Date</th>
-
-	</tr>
-</table>
-
-
 
 <div class="btn-group">
 
@@ -146,4 +66,3 @@
 </div>
 
 @stop
->>>>>>> 96fb6c7f44e0a7f77e141a60816f5f2c4fcb693d
